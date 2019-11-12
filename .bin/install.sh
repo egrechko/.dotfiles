55e51dbf35142ab7b15d09424c49012405856fb5
Eugene Grechko <eugene.grechko@gmail.com>
2019-11-12 15:36:39 -0800

    Added starter install script.

---
+++ .bin/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This install script will install of my required software.
+
+sudo apt update && sudo apt upgrade -y
+
+# Check if stow is installed
+if command -v stow >/dev/null 2>&1; then
+	echo 'stow is installed. Skipping.'
+else
+	sudo apt install stow -y
+fi
