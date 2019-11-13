ec2d0c7100bf7dcc0a0a62e779781d462dd8bf4f
Eugene Grechko <eugene.grechko@gmail.com>
2019-11-13 00:51:51 -0800

    Added patched powerline fonts to install script.

--- .bin/install.sh
+++ .bin/install.sh
@@ -2,14 +2,26 @@
 
 # This install script will install of my required software.
 
-sudo apt update && sudo apt upgrade -y
+echo 'Updating System...'
+sudo apt update >/dev/null 2>&1 && sudo apt upgrade -y >/dev/null 2>&1
+echo 'System Updated.'
 
 # Check if stow is installed
 if command -v stow >/dev/null 2>&1; then
-	echo 'stow is installed. Skipping.'
+	echo 'Skipping: Stow already installed.'
 else
-	sudo apt install stow -y
+	echo 'Installing: stow'
+	sudo apt install stow -y >/dev/null 2>&1
+	echo 'Finished: Installing stow'
 fi
 
+# Install patched powerline font
+if [ -f /usr/share/fonts/opentype/PowerlineSymbols.otf ] && [ -f /etc/fonts/conf.avail/10-powerline-symbols.conf ]; then
+	echo 'Skipping: Patched Powerline Fonts already installed.'
+else
+	echo 'Installing: Patched Powerline Fonts'
+	sudo apt install fonts-powerline >/dev/null 2>&1
+	echo 'Finished: Installing Patched Powerline Fonts'
+fi
 
 # Install neovim
