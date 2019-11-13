bfded5287cd26231a846347354e84b36580ce3ba
Eugene Grechko <eugene.grechko@gmail.com>
2019-11-13 11:51:02 -0800

    Added gnome-terminal theming dep install to install script.

--- .bin/install.sh
+++ .bin/install.sh
@@ -10,7 +10,7 @@ echo 'System Updated.'
 if command -v stow >/dev/null 2>&1; then
 	echo 'Skipping: Stow already installed.'
 else
-	echo 'Installing: stow'
+	echo 'Installing stow...'
 	sudo apt install stow -y >/dev/null 2>&1
 	echo 'Finished: Installing stow'
 fi
@@ -19,9 +19,27 @@ fi
 if [ -f /usr/share/fonts/opentype/PowerlineSymbols.otf ] && [ -f /etc/fonts/conf.avail/10-powerline-symbols.conf ]; then
 	echo 'Skipping: Patched Powerline Fonts already installed.'
 else
-	echo 'Installing: Patched Powerline Fonts'
-	sudo apt install fonts-powerline >/dev/null 2>&1
+	echo 'Installing Patched Powerline Fonts...'
+	sudo apt install fonts-powerline -y >/dev/null 2>&1
 	echo 'Finished: Installing Patched Powerline Fonts'
 fi
 
 # Install neovim
+
+# Install Gogh (gnome-terminal theme) dependencies
+# Gogh github https://github.com/Mayccoll/Gogh
+if dpkg -s dconf-cli >/dev/null 2>&1; then
+	echo 'Skipping: dconf-cli is already installed.'
+else
+	echo 'Installing dconf-cli...'
+	sudo apt install dconf-cli >/dev/null 2>&1
+	echo 'Installed: dconf-cli.'
+fi
+
+if dpkg -s uuid-runtime >/dev/null 2>&1; then
+	echo 'Skipping: uuid-runtime is already installed.'
+else
+	echo 'Installing uuid-runtime...'
+	sudo apt install uuid-runtime -y >/dev/null 2>&1
+	echo 'Installed: uuid-runtime.'
+fi
