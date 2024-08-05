22eea145b606ee2d42675957e7ecac5ceb1c63ee
Eugene Grechko <eugene.grechko@gmail.com>
2024-08-05 14:53:15 -0400

    Added Lua, Bash, & Javascript LSP support.

--- nvim/.config/nvim/lua/e/lsp.lua
+++ nvim/.config/nvim/lua/e/lsp.lua
@@ -1,4 +1,6 @@
 -- This file is used to configure language servers I have installed locally on my machine. I am not using mason to install language servers.
+
+-- Rust Analyzer LSP Support
 require('lspconfig').rust_analyzer.setup {
   -- Other Configs ...
   settings = {
@@ -17,8 +19,51 @@ require('lspconfig').rust_analyzer.setup {
   }
 }
 
--- TODO
--- 1. Setup Language servers
---  a. Javascript
---  b. Lua
---  c. Bash
+-- Lua LSP Support
+-- Future reference: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+require('lspconfig').lua_ls.setup {
+	on_init = function(client)
+	    local path = client.workspace_folders[1].name
+	    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+	      return
+	    end
+
+	    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+	      runtime = {
+	        -- Tell the language server which version of Lua you're using
+	        -- (most likely LuaJIT in the case of Neovim)
+	        version = 'LuaJIT'
+	      },
+	      -- Make the server aware of Neovim runtime files
+	      workspace = {
+	        checkThirdParty = false,
+	        library = {
+	          vim.env.VIMRUNTIME
+	          -- Depending on the usage, you might want to add additional paths here.
+	          -- "${3rd}/luv/library"
+	          -- "${3rd}/busted/library",
+	        }
+	        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+	        -- library = vim.api.nvim_get_runtime_file("", true)
+	      }
+	    })
+	end,
+	settings = {
+	  Lua = {}
+	}
+}
+
+-- Bash LSP Support lol
+require('lspconfig').bashls.setup{}
+
+-- Javascript LSP Support
+-- not attaching to .js buffers
+-- need to create a eslint.config.js or .eslintrc file in the root of a JS project
+require('lspconfig').eslint.setup {
+    on_attach = function(client, bufnr)
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = bufnr,
+        command = "EslintFixAll",
+      })
+    end,
+}
